2d1d3571c3fdd4a6714dda499e03535ccb6ce9ed
Michael Fulgencio <michael.a.fulgencio@gmail.com>
2013-10-13 16:39:00 -0700

    main page done

--- EducationalApp/src/com/example/educationalapp/MainActivity.java
+++ EducationalApp/src/com/example/educationalapp/MainActivity.java
@@ -5,11 +5,14 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	android.widget.Button calcButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		calcButton = (android.widget.Button)findViewById(R.id.calc1_button);
 	}
 
 	@Override
