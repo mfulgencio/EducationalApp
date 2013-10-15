a50a8e247a1b3d797166ac99b5923e9c298adb95
Michael Fulgencio <michael.a.fulgencio@gmail.com>
2013-10-15 16:16:41 -0700

    added topics page

--- EducationalApp/src/com/example/educationalapp/MainActivity.java
+++ EducationalApp/src/com/example/educationalapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.educationalapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	android.widget.Button calcButton;
 
 	@Override
@@ -13,6 +16,15 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		
 		calcButton = (android.widget.Button)findViewById(R.id.calc1_button);
+		
+		calcButton.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		Intent i = new Intent(this, Topics.class);
+		
+		this.startActivity(i);
 	}
 
 	@Override
