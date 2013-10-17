baaa424382234c4169302a29b7d7604038927c40
Michael Fulgencio <michael.a.fulgencio@gmail.com>
2013-10-16 21:51:56 -0700

    topics page updated

--- EducationalApp/src/com/example/educationalapp/Topics.java
+++ EducationalApp/src/com/example/educationalapp/Topics.java
@@ -11,6 +11,7 @@ public class Topics  extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.topics);
 	
+		//limits spinner
 		Spinner limits_spinner = (Spinner) findViewById(R.id.limit_spinner);
 		
 		ArrayAdapter<CharSequence> limits_adapter = ArrayAdapter.createFromResource(this, R.array.limits_array, android.R.layout.simple_spinner_item);
@@ -18,5 +19,23 @@ public class Topics  extends Activity {
 		limits_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		
 		limits_spinner.setAdapter(limits_adapter);
+		
+		//derivatives spinner
+		Spinner derivatives_spinner = (Spinner) findViewById(R.id.derivatives_spinner);
+		
+		ArrayAdapter<CharSequence> derivatives_adapter = ArrayAdapter.createFromResource(this, R.array.derivatives_array, android.R.layout.simple_spinner_item);
+		
+		derivatives_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		derivatives_spinner.setAdapter(derivatives_adapter);
+		
+		//integrals spinner
+				Spinner integrals_spinner = (Spinner) findViewById(R.id.integrals_spinner);
+				
+				ArrayAdapter<CharSequence> integrals_adapter = ArrayAdapter.createFromResource(this, R.array.integrals_array, android.R.layout.simple_spinner_item);
+				
+				integrals_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				
+				integrals_spinner.setAdapter(integrals_adapter);
 	}
 }
