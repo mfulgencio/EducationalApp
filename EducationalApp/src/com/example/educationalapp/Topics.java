dcb27b5c1807e54a70e67437dd0c680f3c9bdebb
mgfeldman <MarkFeldman@pcp075572pcs.wireless.calpoly.edu>
2013-10-20 16:52:27 -0700

    Spinner sizes changed

--- EducationalApp/src/com/example/educationalapp/Topics.java
+++ EducationalApp/src/com/example/educationalapp/Topics.java
@@ -19,27 +19,27 @@ public class Topics  extends Activity implements OnClickListener {
 		//limits spinner
 		Spinner limits_spinner = (Spinner) findViewById(R.id.limit_spinner);
 		
-		ArrayAdapter<CharSequence> limits_adapter = ArrayAdapter.createFromResource(this, R.array.limits_array, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> limits_adapter = ArrayAdapter.createFromResource(this, R.array.limits_array, R.layout.custom_spinner_list);
 		
-		limits_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		limits_adapter.setDropDownViewResource(R.layout.custom_spinner);
 		
 		limits_spinner.setAdapter(limits_adapter);
 		
 		//derivatives spinner
 		Spinner derivatives_spinner = (Spinner) findViewById(R.id.derivatives_spinner);
 		
-		ArrayAdapter<CharSequence> derivatives_adapter = ArrayAdapter.createFromResource(this, R.array.derivatives_array, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> derivatives_adapter = ArrayAdapter.createFromResource(this, R.array.derivatives_array, R.layout.custom_spinner_list);
 		
-		derivatives_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		derivatives_adapter.setDropDownViewResource(R.layout.custom_spinner);
 		
 		derivatives_spinner.setAdapter(derivatives_adapter);
 		
 		//integrals spinner
 		Spinner integrals_spinner = (Spinner) findViewById(R.id.integrals_spinner);
 		
-		ArrayAdapter<CharSequence> integrals_adapter = ArrayAdapter.createFromResource(this, R.array.integrals_array, android.R.layout.simple_spinner_item);
+		ArrayAdapter<CharSequence> integrals_adapter = ArrayAdapter.createFromResource(this, R.array.integrals_array,R.layout.custom_spinner_list);
 		
-		integrals_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		integrals_adapter.setDropDownViewResource(R.layout.custom_spinner);
 		
 		integrals_spinner.setAdapter(integrals_adapter);
 		
