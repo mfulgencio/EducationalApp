41939a46f02295283829aec6147d1f279a59c8df
Michael Fulgencio <michael.a.fulgencio@gmail.com>
2013-11-03 14:14:38 -0800

    pushing youtube api

---
+++ YouTubeAndroidPlayerApi-1.0.0/docs/reference/lists.js
@@ -0,0 +1,26 @@
+var DATA = [
+      { id:0, label:"com.google.android.youtube.player", link:"com/google/android/youtube/player/package-summary.html", type:"package" },
+      { id:1, label:"com.google.android.youtube.player.YouTubeApiServiceUtil", link:"com/google/android/youtube/player/YouTubeApiServiceUtil.html", type:"class" },
+      { id:2, label:"com.google.android.youtube.player.YouTubeBaseActivity", link:"com/google/android/youtube/player/YouTubeBaseActivity.html", type:"class" },
+      { id:3, label:"com.google.android.youtube.player.YouTubeInitializationResult", link:"com/google/android/youtube/player/YouTubeInitializationResult.html", type:"class" },
+      { id:4, label:"com.google.android.youtube.player.YouTubeIntents", link:"com/google/android/youtube/player/YouTubeIntents.html", type:"class" },
+      { id:5, label:"com.google.android.youtube.player.YouTubePlayer", link:"com/google/android/youtube/player/YouTubePlayer.html", type:"class" },
+      { id:6, label:"com.google.android.youtube.player.YouTubePlayer.ErrorReason", link:"com/google/android/youtube/player/YouTubePlayer.ErrorReason.html", type:"class" },
+      { id:7, label:"com.google.android.youtube.player.YouTubePlayer.OnFullscreenListener", link:"com/google/android/youtube/player/YouTubePlayer.OnFullscreenListener.html", type:"class" },
+      { id:8, label:"com.google.android.youtube.player.YouTubePlayer.OnInitializedListener", link:"com/google/android/youtube/player/YouTubePlayer.OnInitializedListener.html", type:"class" },
+      { id:9, label:"com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener", link:"com/google/android/youtube/player/YouTubePlayer.PlaybackEventListener.html", type:"class" },
+      { id:10, label:"com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener", link:"com/google/android/youtube/player/YouTubePlayer.PlayerStateChangeListener.html", type:"class" },
+      { id:11, label:"com.google.android.youtube.player.YouTubePlayer.PlayerStyle", link:"com/google/android/youtube/player/YouTubePlayer.PlayerStyle.html", type:"class" },
+      { id:12, label:"com.google.android.youtube.player.YouTubePlayer.PlaylistEventListener", link:"com/google/android/youtube/player/YouTubePlayer.PlaylistEventListener.html", type:"class" },
+      { id:13, label:"com.google.android.youtube.player.YouTubePlayer.Provider", link:"com/google/android/youtube/player/YouTubePlayer.Provider.html", type:"class" },
+      { id:14, label:"com.google.android.youtube.player.YouTubePlayerFragment", link:"com/google/android/youtube/player/YouTubePlayerFragment.html", type:"class" },
+      { id:15, label:"com.google.android.youtube.player.YouTubePlayerSupportFragment", link:"com/google/android/youtube/player/YouTubePlayerSupportFragment.html", type:"class" },
+      { id:16, label:"com.google.android.youtube.player.YouTubePlayerView", link:"com/google/android/youtube/player/YouTubePlayerView.html", type:"class" },
+      { id:17, label:"com.google.android.youtube.player.YouTubeStandalonePlayer", link:"com/google/android/youtube/player/YouTubeStandalonePlayer.html", type:"class" },
+      { id:18, label:"com.google.android.youtube.player.YouTubeThumbnailLoader", link:"com/google/android/youtube/player/YouTubeThumbnailLoader.html", type:"class" },
+      { id:19, label:"com.google.android.youtube.player.YouTubeThumbnailLoader.ErrorReason", link:"com/google/android/youtube/player/YouTubeThumbnailLoader.ErrorReason.html", type:"class" },
+      { id:20, label:"com.google.android.youtube.player.YouTubeThumbnailLoader.OnThumbnailLoadedListener", link:"com/google/android/youtube/player/YouTubeThumbnailLoader.OnThumbnailLoadedListener.html", type:"class" },
+      { id:21, label:"com.google.android.youtube.player.YouTubeThumbnailView", link:"com/google/android/youtube/player/YouTubeThumbnailView.html", type:"class" },
+      { id:22, label:"com.google.android.youtube.player.YouTubeThumbnailView.OnInitializedListener", link:"com/google/android/youtube/player/YouTubeThumbnailView.OnInitializedListener.html", type:"class" }
+
+    ];
